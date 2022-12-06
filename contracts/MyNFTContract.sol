3085b57d68a0d465954062a9b322924bed1a8755
Achilleas <a.michos.ext@tradias.de>
2022-12-06 16:04:25 +0100

    add event

--- contracts/MyNFTContract.sol
+++ contracts/MyNFTContract.sol
@@ -13,6 +13,8 @@ contract MyNFTContract is ERC721URIStorage {
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
 
+  event NewNFTContractMinted(address sender, uint256 tokenId);
+
   string baseSvg = "<svg xmlns='http://www.w3.org/2000/svg' preserveAspectRatio='xMinYMin meet' viewBox='0 0 350 350'><style>.base { fill: white; font-family: serif; font-size: 24px; }</style><rect width='100%' height='100%' fill='black' /><text x='50%' y='50%' class='base' dominant-baseline='middle' text-anchor='middle'>";
 
   constructor() ERC721 ("AchilleasNFT", "ACH") {
@@ -52,6 +54,7 @@ contract MyNFTContract is ERC721URIStorage {
     _setTokenURI(newItemId, finalTokenUri);
   
     console.log("An NFT w/ ID %s has been minted to %s", newItemId, msg.sender);
+    emit NewNFTContractMinted(msg.sender, newItemId);
     _tokenIds.increment();
   }
 }
